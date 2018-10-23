9c49fe2f67127e4f9fe598e3894dc626084e78d9
dr1zzt2 <dr1zzt2.NK@gmail.com>
2018-10-23 20:55:30 +0200

    implemented classes according to the UML

---
+++ src/com/codecool/uml/overriding/Order.java
@@ -0,0 +1,34 @@
+package com.codecool.uml.overriding;
+
+public class Order implements Orderable {
+    private static int ID_COUNTER = 0;
+    private int id;
+    private String status;
+
+    public Order() {
+        this.id = ID_COUNTER++;
+        this.status = "new";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public boolean checkout() {
+        if (this.status.equals("new")) {
+            this.status = "checked";
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean pay() {
+        if (this.status.equals("checked")) {
+            this.status = "payed";
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
